07ded6eceebdfc2bef128e6190456da9b4cef003
syedfaizz <syedfaizz45@gmail.com>
2021-09-06 16:35:18 +0530

    [faisal] Add: uc5

--- EmployeeOperation.cs
+++ EmployeeOperation.cs
@@ -84,5 +84,58 @@ namespace EmployeePayroll
                 this.sqlConnection.Close();
             }
         }
+        public void GetEmployeeDetailsByDate()
+        {
+            Employee employee = new Employee();
+            DateTime startDate = new DateTime(1997, 01, 10);
+            DateTime endDate = new DateTime(2010, 04, 15);
+            try
+            {
+                this.sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("spGetDataByDate", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@StartDate", startDate);
+                sqlCommand.Parameters.AddWithValue("@EndDate", endDate);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        employee.ID = reader.GetInt32(0);
+                        employee.Name = reader.GetString(1);
+                        employee.Gender = reader.GetString(2);
+                        employee.PhoneNumber = reader.GetString(3);
+                        employee.Address = reader.GetString(4);
+                        employee.Department = reader.GetString(5);
+                        employee.BasicPay = reader.GetInt32(6);
+                        employee.Deduction = reader.GetDouble(7);
+                        employee.TaxablePay = reader.GetDouble(8);
+                        employee.Tax = reader.GetDouble(9);
+                        employee.NetPay = reader.GetDouble(10);
+                        employee.StartDate = reader.GetDateTime(11);
+                        employee.City = reader.GetString(12);
+                        employee.Country = reader.GetString(13);
+                        //// display the payroll data from database
+                        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13}", employee.ID, employee.Name,
+                            employee.Gender, employee.PhoneNumber, employee.Address, employee.Department, employee.BasicPay, employee.Deduction,
+                            employee.TaxablePay, employee.Tax, employee.NetPay, employee.StartDate, employee.City, employee.Country);
+                        Console.WriteLine("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No record found");
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.sqlConnection.Close();
+            }
+        }
     }
 }
