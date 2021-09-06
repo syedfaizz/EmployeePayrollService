36992e260380c1141719be0ca6d8a5f10abb3860
syedfaizz <syedfaizz45@gmail.com>
2021-09-06 16:22:27 +0530

    [faisal] Add: uc3

--- EmployeeOperation.cs
+++ EmployeeOperation.cs
@@ -11,44 +11,40 @@ namespace EmployeePayroll
         public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service";
         SqlConnection sqlConnection = new SqlConnection(connectionString);
         public static Employee employee = new Employee();
-        public void GetAllEmployeeDetail()
+        readonly List<Employee> empList = new List<Employee>();
+        public List<Employee> GetAllEmployeeDetails()
         {
             this.sqlConnection.Open();
-            string query = "SELECT * FROM dbo.employee_payroll";
             try
             {
-                SqlCommand command = new SqlCommand(query, sqlConnection);
-                SqlDataReader reader = command.ExecuteReader();
-                if (reader.HasRows)
+                SqlCommand command = new SqlCommand("spGetAllEmployee", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                foreach (DataRow data in dataTable.Rows)
                 {
-                    while (reader.Read())
-                    {
-                        employee.ID = reader.GetInt32(0);
-                        employee.Name = reader.GetString(1);
-                        employee.Gender = reader.GetString(2);
-                        employee.PhoneNumber = reader.GetString(3);
-                        employee.Address = reader.GetString(4);
-                        employee.Department = reader.GetString(5);
-                        employee.BasicPay = reader.GetInt32(6);
-                        employee.Deduction = reader.GetDouble(7);
-                        employee.TaxablePay = reader.GetDouble(8);
-                        employee.Tax = reader.GetDouble(9);
-                        employee.NetPay = reader.GetDouble(10);
-                        employee.StartDate = reader.GetDateTime(11);
-                        employee.City = reader.GetString(12);
-                        employee.Country = reader.GetString(13);
-                        //// display the payroll data from database
-                        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13}", employee.ID, employee.Name,
-                            employee.Gender, employee.PhoneNumber, employee.Address, employee.Department, employee.BasicPay, employee.Deduction,
-                            employee.TaxablePay, employee.Tax, employee.NetPay, employee.StartDate, employee.City, employee.Country);
-                        Console.WriteLine("\n");
-                    }
+                    empList.Add
+                        (
+                            new Employee
+                            {
+                                ID = Convert.ToInt32(data["Id"]),
+                                Name = Convert.ToString(data["Name"]),
+                                Gender = Convert.ToString(data["Gender"]),
+                                PhoneNumber = Convert.ToString(data["PhoneNumber"]),
+                                Address = Convert.ToString(data["Address"]),
+                                Department = Convert.ToString(data["Department"]),
+                                BasicPay = Convert.ToInt32(data["BasicPay"]),
+                                Deduction = Convert.ToDouble(data["Deduction"]),
+                                TaxablePay = Convert.ToDouble(data["TaxablePay"]),
+                                Tax = Convert.ToDouble(data["Tax"]),
+                                NetPay = Convert.ToDouble(data["NetPay"]),
+                                StartDate = Convert.ToDateTime(data["StartDate"]),
+                                City = Convert.ToString(data["City"]),
+                                Country = Convert.ToString(data["Country"])
+                            }
+                        );
                 }
-                else
-                {
-                    Console.WriteLine("No record found");
-                }
-                reader.Close();
             }
             catch (Exception e)
             {
@@ -58,6 +54,7 @@ namespace EmployeePayroll
             {
                 this.sqlConnection.Close();
             }
+            return empList;
         }
     }
 }
